6546c6b971a08c20050bfb70b8b8739b2e16a917
alexandraelizabethpominova-wq <alexandra.elizabeth.pominova@gmail.com>
2026-09-05 16:30:06 -0400

    Add AlexTextField MUI wrapper

---
+++ src/design-system/atoms/AlexTextField.tsx
@@ -0,0 +1,5 @@
+import {TextField,type TextFieldProps} from '@mui/material'
+
+export default function AlexTextField(props:TextFieldProps){
+  return <TextField fullWidth size="small" {...props}/>
+}
