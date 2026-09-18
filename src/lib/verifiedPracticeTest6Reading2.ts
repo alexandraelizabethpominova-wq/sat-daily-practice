349b877f7edbdc16ccab905eac3cdad73eabb0f3
alexandraelizabethpominova-wq <alexandra.elizabeth.pominova@gmail.com>
2026-09-18 14:44:31 -0400

    Parse Practice Test 6 Reading Module 2 Q3

--- src/lib/verifiedPracticeTest6Reading2.ts
+++ src/lib/verifiedPracticeTest6Reading2.ts
@@ -16,6 +16,13 @@ export const VERIFIED_PRACTICE_TEST_6_READING2:Record<number,VerifiedPracticeTes
     'B) uncertain about',
     'C) unbothered by',
     'D) inspired by',
+  ]),  3:q([
+    'Animal researcher Amalia P.M. Bastos led a 2021 study about a wild kea parrot that used small stones as tools to preen its feathers. Skeptical colleagues had initially suggested to Bastos that the kea’s interactions with the stones might simply be _______ , but Bastos and her team showed that the kea was using the stones deliberately.',
+    'Which choice completes the text with the most logical and precise word or phrase?',
+    'A) intriguing',
+    'B) obvious',
+    'C) accidental',
+    'D) observable',
   ]),
 }
 
