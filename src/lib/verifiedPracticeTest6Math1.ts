bd6dee4703544e4603d0bf70b404061162627a05
alexandraelizabethpominova-wq <alexandra.elizabeth.pominova@gmail.com>
2026-09-18 15:07:49 -0400

    Parse Practice Test 6 Math Module 1 Q21

--- src/lib/verifiedPracticeTest6Math1.ts
+++ src/lib/verifiedPracticeTest6Math1.ts
@@ -164,7 +164,9 @@ export const VERIFIED_PRACTICE_TEST_6_MATH1:Record<number,VerifiedPracticeTest6M
 ,
   ]),  20:q([
     'What is the $y$-coordinate of the $y$-intercept of the graph of $\\frac{3x}{7}=-\\frac{5y}{9}+21$ in the $xy$-plane?',
-  ]),
+  ]),  21:q([
+    'The graph of $y=2x^2+bx+c$ is shown, where $b$ and $c$ are constants. What is the value of $bc$?',
+  ],true),
 }
 
 export function verifiedPracticeTest6Math1Content(questionNumber:number){return VERIFIED_PRACTICE_TEST_6_MATH1[questionNumber]}
