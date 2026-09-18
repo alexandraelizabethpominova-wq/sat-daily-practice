da160e81fa268bf33e3f166a806f99634c3d4315
alexandraelizabethpominova-wq <alexandra.elizabeth.pominova@gmail.com>
2026-09-18 13:48:06 -0400

    Update sidebar test for parsing issues navigation

--- src/design-system/organisms/AppSidebarLayout.test.tsx
+++ src/design-system/organisms/AppSidebarLayout.test.tsx
@@ -16,6 +16,7 @@ describe('AppSidebarLayout',()=>{
         onPracticeTests={()=>undefined}
         onPracticeSetup={onPracticeSetup}
         onQuestionBank={onQuestionBank}
+        onParsingIssues={()=>undefined}
         onPerformance={()=>undefined}
         onResources={()=>undefined}
         onSettings={()=>undefined}
