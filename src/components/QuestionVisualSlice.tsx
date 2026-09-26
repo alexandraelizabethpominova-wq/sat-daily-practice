73063b315bee7043a02c4e75875c42c4ba5371a6
alexandraelizabethpominova-wq <alexandra.elizabeth.pominova@gmail.com>
2026-09-26 11:15:18 -0400

    Restore known-good PDF.js worker setup

--- src/components/QuestionVisualSlice.tsx
+++ src/components/QuestionVisualSlice.tsx
@@ -1,5 +1,7 @@
 import {useEffect,useState} from 'react'
-import {getDocument,type PDFDocumentProxy} from '../lib/pdfJs'
+
+GlobalWorkerOptions.workerSrc=new URL('pdfjs-dist/build/pdf.worker.min.mjs',import.meta.url).toString()
+import {GlobalWorkerOptions,getDocument,type PDFDocumentProxy} from 'pdfjs-dist'
 import {getQuestionImage,saveQuestionImage} from '../lib/questionImageStore'
 import {questionCropForParts} from '../lib/questionCrops'
 import {expandNormalizedCrop,type NormalizedCrop} from '../lib/questionVisuals'
